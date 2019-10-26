70e66e0a91a020c6a5732db195c129d209839c76
jaywiles <jay.wiles@gmail.com>
2019-10-26 09:54:29 -0500

    more progress

--- src/App.js
+++ src/App.js
@@ -18,24 +18,41 @@ class App extends Component {
   fetchData() {
     fetch ('https://api.punkapi.com/v2/beers')
     .then(response => response.json())
-    .then(parsedJSON => parsedJSON.results.map(beer => ({
+    // .then(parsedJSON => console.log(parsedJSON.map(beer => (
+    //   {
+    //     name: `${beer.name}`
+    //   }
+    // ))))
+    .then(parsedJSON => parsedJSON.map(beer => ({
       name: `${beer.name}`,
       tagline: `${beer.tagline}`,
       abv: `${beer.abv}`,
       ibu: `${beer.ibu}`,
-      mash_temp_value_c: `${beer.method.mash_temp.temp.value}`
+      // mash_temp_value_c: `${beer.method.mash_temp.temp.value}`
     })))
     .catch(error => console.log("parsing failed", error))
-  }
+    }
 
   render() {
-    // return (console.log('hi'))
+  //   // return (console.log('hi'))
     const {isLoading, beers} = this.state;
     return (
       <div>
         <header>Good Beers</header>
-        <div className={`content ${isLoading ? 'is-loading' : '' } `}>
-
+        <div className={`content ${isLoading ? 'is-loading' : ''}`}>
+          <div className="beer-info">
+            {
+              // how to do if statement everything is wrapped in????
+              !isLoading && beers.length ? beers.map(beer => {
+                const {name, tagline, abv, ibu} = beer;
+                return <div> key={beer} title={name}>
+                  <p>Tagline: {tagline}</p>
+                  <p>ABV: {abv}</p>
+                  <p>IBU: {ibu}</p>
+                  </div>
+              }) : null
+            }
+          </div>
         </div>
       </div>
     )
